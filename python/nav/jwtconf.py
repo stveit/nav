1c39e88f1cf1e3920383eeaca6b13879d183ce66
Simon Oliver Tveit <simtve@uninett.no>
2023-05-25 09:25:47 +0200

    Add funcs for reading local jwt config options

--- python/nav/jwtconf.py
+++ python/nav/jwtconf.py
@@ -11,6 +11,7 @@ class JWTConf(NAVConfigParser):
     """jwt.conf config parser"""
 
     DEFAULT_CONFIG_FILES = ('jwt.conf',)
+    NAV_SECTION = "nav"
 
     def get_issuers_setting(self):
         issuers_settings = dict()
@@ -21,7 +22,7 @@ class JWTConf(NAVConfigParser):
                 aud = self._validate_audience(get('aud'))
                 key_type = self._validate_type(get('keytype'))
                 if key_type == 'PEM':
-                    key = self._read_file(key)
+                    key = self._read_key_from_path(key)
                 claims_options = {
                     'aud': {'values': [aud], 'essential': True},
                 }
@@ -34,9 +35,18 @@ class JWTConf(NAVConfigParser):
                 _logger.error('Error collecting stats for %s: %s', section, error)
         return issuers_settings
 
-    def _read_file(self, file):
-        with open(file, "r") as f:
-            return f.read()
+    def _read_key_from_path(self, path):
+        try:
+            with open(path, "r") as f:
+                return f.read()
+        except FileNotFoundError:
+            raise ConfigurationError(
+                "Could not find file %s to read PEM key from", path
+            )
+        except PermissionError:
+            raise ConfigurationError(
+                "Could not access file %s to read PEM key from", path
+            )
 
     def _validate_key(self, key):
         if not key:
@@ -54,3 +64,27 @@ class JWTConf(NAVConfigParser):
         if not audience:
             raise ConfigurationError("Invalid 'aud': 'aud' must not be empty")
         return audience
+
+    def _get_nav_token_config_option(self, option):
+        try:
+            get = partial(self.get, self.NAV_SECTION)
+            return get(option)
+        except (
+            configparser.NoSectionError,
+            configparser.NoOptionError,
+        ) as error:
+            raise ConfigurationError(error)
+
+    def get_nav_private_key(self):
+        path = self._get_nav_token_config_option('private_key')
+        return self._read_key_from_path(path)
+
+    def get_nav_public_key(self):
+        path = self._get_nav_token_config_option('public_key')
+        return self._read_key_from_path(path)
+
+    def get_nav_name(self):
+        name = self._get_nav_token_config_option('name')
+        if not name:
+            raise ConfigurationError("Invalid 'name': 'name' must not be empty")
+        return name
