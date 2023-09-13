77b827c38732d6d01be52297c0d1a1d6036b2cea
Simon Oliver Tveit <simtve@uninett.no>
2023-09-13 13:05:22 +0200

    delete

--- tests/unittests/portadmin/portadmin_poe_juniper_test.py
+++
@@ -1,7 +0,0 @@
-from nav.portadmin.snmp.juniper import Juniper
-
-
-def test_returns_correct_state_options(handler_juniper):
-    state_options = handler_juniper.get_poe_state_options()
-    assert Juniper.POE_ENABLED in state_options
-    assert Juniper.POE_DISABLED in state_options
